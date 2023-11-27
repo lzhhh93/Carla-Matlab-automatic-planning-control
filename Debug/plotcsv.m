91ae0b029d1d3de681bec8aa10cc0e0d5543e58c
Samuel Leung <38736831+lzhhh93@users.noreply.github.com>
2023-11-27 08:52:41 +0100

    Delete Debug directory

--- Debug/plotcsv.m
+++
@@ -1,7 +0,0 @@
-Location = readmatrix('e.csv')
-col1 = Location(:, 1);
-col2 = Location(:, 2);
-Location1 = readmatrix('m.csv')
-col3 = Location1(:, 1);
-col4 = Location1(:, 2);
-plot(col1, col2, 'r-o', col3, col4, 'b-+' )
